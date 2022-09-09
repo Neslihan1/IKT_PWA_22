725f4bc7a0cb854902a19f7f3b9e7a008619af2c
Neslihan1 <nesli_97@hotmail.de>
2022-09-09 20:27:29 +0200

    service worker installing

--- public/sw.js
+++ public/sw.js
@@ -6,3 +6,13 @@ importScripts(
       ({request}) => request.destination === 'image',
       new workbox.strategies.NetworkFirst()     // NetworkFirst() vs CacheFirst()
   )
+  
+
+  self.addEventListener('install', function(event) {
+    console.log('service worker --> installing ...', event);
+})
+
+self.addEventListener('activate', function(event) {
+    console.log('service worker --> activating ...', event);
+    return self.clients.claim();
+})
