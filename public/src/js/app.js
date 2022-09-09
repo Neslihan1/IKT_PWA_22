a242ac4b15114b5e0abb1c0f5d15cfb5b1d65c8d
Neslihan1 <nesli_97@hotmail.de>
2022-09-09 22:32:39 +0200

    fetch Ereignis

--- public/src/js/app.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-        .register('/sw.js')
-        .then(function() {
-            console.log('service worker registriert')
-        });
-}
--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,12 @@
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
-  );
-  
-  workbox.routing.registerRoute(
-      ({request}) => request.destination === 'image',
-      new workbox.strategies.NetworkFirst()     // NetworkFirst() vs CacheFirst()
-  )
-  
-
-  self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     console.log('service worker --> installing ...', event);
 })
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
     console.log('service worker --> activating ...', event);
     return self.clients.claim();
+})
+
+self.addEventListener('fetch', event => {
+    console.log('service worker --> fetching ...', event);
 })
